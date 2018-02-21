3942382eefafcb238fbb630a994c1f4ed5a7928c
Aasharib <mnawshad.bese15seecs@seecs.edu.pk>
2018-02-21 07:29:24 +0500

    Software Construction LAB 2 Initial Commit

---
+++ Library Management System/AddArtifact.cs
@@ -0,0 +1,229 @@
+﻿//Done By Aasharib
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Library_Management_System
+{
+    public partial class Form1 : Form
+    {
+        String sqlConName = "datasource=localhost;port=3306;username=root;password=";
+        //String queryToExecute = "insert into library_system.studentinfo(idStudentInfo,Name,Father_Name,Age,Semester) values('" + this.IdTextBox.Text + "','" + this.NameTextBox.Text + "','" + this.FnameTextBox.Text + "','" + this.AgeTextBox.Text + "','" + this.SemesterTextBox.Text + "');";
+
+        public Form1()
+        {
+            InitializeComponent();
+            initInputBox();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String publishDate = dateTimePicker1.Value.ToString("yyyy-MM-dd");
+            String status = comboBox1.Text.ToString();
+            String batchNo = textBox3.Text.ToString();
+            String price = textBox4.Text.ToString();
+            String mode = comboBox2.Text.ToString();
+            String title = comboBox5.Text.ToString();
+            String author = textBox7.Text.ToString();
+            String artifact = comboBox3.Text.ToString();
+            String quantity = textBox9.Text.ToString();
+            String genre = comboBox4.Text.ToString();
+            
+            int quantityB = Convert.ToInt32(quantity);
+            int titleCheck = checkForeignKeyValid("library_system", "name", "id", "title",title);
+            int artifactCheck = checkForeignKeyValid("library_system", "name", "id", "artifact",artifact);
+            int genreCheck = checkForeignKeyValid("library_system", "name", "id", "genre", genre);
+            int authorCheck = checkForeignKeyValid("library_system", "name", "id", "author", author);
+
+            //if title is not available in title table therefore foreign key cannot be referenced
+            if(titleCheck == 0) {
+                MySqlConnection con1 = connectToDb();
+                String query1 = "insert into " + "library_system" + "." + "title (" + "name" + ")" + "values ('"
+                            + title + "');";
+                MySqlCommand command1 = new MySqlCommand(query1, con1);
+                try
+                {
+                    MySqlDataReader sqd = command1.ExecuteReader();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                closeDbConnection(con1);
+                titleCheck = checkForeignKeyValid("library_system", "name", "id", "title", title);
+            }
+
+            //if artifact is not available in artifact table therefore foreign key cannot be referenced
+            if (artifactCheck == 0) {
+                MySqlConnection con2 = connectToDb();
+                String query2 = "insert into " + "library_system" + "." + "artifact (" + "name" + ")" + "values ('"
+                            + artifact + "');";
+                MySqlCommand command2 = new MySqlCommand(query2, con2);
+                try
+                {
+                    MySqlDataReader sqd = command2.ExecuteReader();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                closeDbConnection(con2);
+                artifactCheck = checkForeignKeyValid("library_system", "name", "id", "artifact", artifact);
+            }
+
+            //if genre is not available in genre table therefore foreign key cannot be referenced
+            if (genreCheck == 0) {
+                MySqlConnection con3 = connectToDb();
+                String query3 = "insert into " + "library_system" + "." + "genre (" + "name" + ")" + "values ('"
+                            + genre + "');";
+                MySqlCommand command3 = new MySqlCommand(query3, con3);
+                try
+                {
+                    MySqlDataReader sqd = command3.ExecuteReader();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                closeDbConnection(con3);
+                genreCheck = checkForeignKeyValid("library_system", "name", "id", "genre", genre);
+            }
+
+            //if author is not available in author table therefore foreign key cannot be referenced
+            if (authorCheck == 0) {
+                MySqlConnection con4 = connectToDb();
+                String query4 = "insert into " + "library_system" + "." + "author (" + "name" + ")" + "values ('"
+                            + author + "');";
+                MySqlCommand command4 = new MySqlCommand(query4, con4);
+                try
+                {
+                    MySqlDataReader sqd = command4.ExecuteReader();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                closeDbConnection(con4);
+                authorCheck = checkForeignKeyValid("library_system", "name", "id", "Author", author);
+            }
+
+            //Now performing the insertion operations for addition
+            MySqlConnection con = connectToDb();
+            MySqlDataReader sqd1;
+            for (int i = 0; i < quantityB; i++)
+            {
+                String query = "insert into " + "library_system" + "." + "book (" + "publish_date" + "," + "status" +
+                                "," + "batch_number" + "," + "price" + "," + "mode" + "," + "title_id" + "," + "author_id" +
+                                "," + "genre_id" + "," + "artifact_id )" + "values ('"
+                                + publishDate + "','" + status + "','" + batchNo + "','" + price + "','" + mode + "','" + titleCheck + "','"
+                                + authorCheck + "','" + genreCheck + "','" + artifactCheck + "');";
+                MySqlCommand command = new MySqlCommand(query, con);
+                try
+                {
+                   sqd1 = command.ExecuteReader();
+                   sqd1.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+            }
+            closeDbConnection(con);
+            MessageBox.Show("Data Saved Succesfully!");
+        }
+
+        private void label3_Click(object sender, EventArgs e){}
+
+        private void initInputBox()
+        {
+            loadCombo(comboBox3, "library_system", "artifact", "name", "id");
+            loadCombo(comboBox4, "library_system", "genre", "name", "id");
+            loadCombo(comboBox5, "library_system", "title", "name", "id");
+        }
+
+        private int checkForeignKeyValid(String dbName, String keyColumn,String valColumn, String tableName, String toFind) {
+            MySqlConnection con = connectToDb();
+            String query = "select " + keyColumn + "," + valColumn + " from " + dbName + "." + tableName + ";";
+            MySqlCommand command = new MySqlCommand(query, con);
+            int found = 0;
+            try
+            {
+                MySqlDataReader sqd = command.ExecuteReader();
+                while (sqd.Read())
+                {
+                    int gues = Convert.ToInt32(sqd[valColumn].ToString());
+                    if (sqd[keyColumn].ToString() == toFind) {
+                        found = gues;
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                closeDbConnection(con);
+                return -1;
+            }
+            closeDbConnection(con);
+            return found;
+        }
+
+        // give combobox name , database name, db table name, name of attribute to show and value to bind to attribute
+        private void loadCombo(ComboBox comboName, String dbName, String tableName, String keyColumn, String valColumn) {
+            MySqlConnection con = connectToDb();
+            String query = "select " + keyColumn + "," + valColumn + " from " + dbName + "." + tableName + ";";
+            MySqlCommand command = new MySqlCommand(query, con);
+            try
+            {
+                MySqlDataReader sqd = command.ExecuteReader();
+                Dictionary<String, String> dtSource = new Dictionary<String, String>();
+                while (sqd.Read())
+                {
+                    dtSource.Add(sqd[keyColumn].ToString(), sqd[valColumn].ToString());
+                }
+                comboName.DataSource = new BindingSource(dtSource, null);
+                comboName.DisplayMember = "Key";
+                comboName.ValueMember = "Value";
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            closeDbConnection(con);
+        }
+
+        private MySqlConnection connectToDb() {
+            try
+            {
+                MySqlConnection con = new MySqlConnection(sqlConName);
+                con.Open();
+                if (con.State == ConnectionState.Open) {
+                    
+                }
+                return con;
+            }
+
+            catch (Exception ex){
+                MessageBox.Show(ex.Message);               
+                return null;
+            }
+        }
+
+        private void closeDbConnection(MySqlConnection con){
+            con.Close();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Library Management System/SearchArtifact.cs
@@ -0,0 +1,159 @@
+﻿//Done By Aasharib
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Library_Management_System
+{
+    public partial class SearchArtifact : Form
+    {
+
+        String sqlConName = "datasource=localhost;port=3306;username=root;password=";
+        public SearchArtifact()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String authorName = textBox8.Text;
+            MySqlConnection con = connectToDb();
+
+            /*String tbName = "author";
+            String dbName = "library_system";
+            String valColumn = "id";*/
+            //String keyColumn = "name";
+            //String toFind = "";
+            //select title.name, author.name from book join author join title on book.title_id = title.id and book.author_id = author.id
+            //String query = "select " + "*" + " from " + dbName + "." + tbName +" " +"where " + "name= " + "\'"+ authorName+"\'" + ";";
+
+            String query = "select library_system.book.id,library_system.title.name as title, library_system.author.name as author,library_system.book.publish_date, library_system.book.batch_number, library_system.book.price,library_system.book.mode as mode, library_system.genre.name as genre, library_system.artifact.name as artifact from library_system.book join library_system.author join library_system.title join library_system.genre join library_system.artifact on book.title_id = title.id and book.author_id = author.id and book.genre_id = genre.id and book.artifact_id = artifact.id where author.name = " + "\'" + authorName + "\'" + ";";
+
+           // MySqlCommand command = new MySqlCommand(query, con);
+            try
+            {
+              //  MySqlDataReader sqd = command.ExecuteReader();
+              //  sqd.Read();
+              //  int gotId = Convert.ToInt32(sqd[valColumn].ToString());
+              //  sqd.Close();
+              //  String query2 = "select " + "*" + " from " + dbName + "." + "book" + " " + "where " + "author_id = " + gotId;
+
+                MySqlDataAdapter command2 = new MySqlDataAdapter(query, con);
+                DataSet gatherData = new DataSet();
+                command2.Fill(gatherData);
+                dataGridView1.DataSource = gatherData.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                closeDbConnection(con);
+            }
+            closeDbConnection(con);
+        }
+
+        private MySqlConnection connectToDb()
+        {
+            try
+            {
+                MySqlConnection con = new MySqlConnection(sqlConName);
+                con.Open();
+                if (con.State == ConnectionState.Open)
+                {
+                    
+                }
+                return con;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+        }
+
+        private void closeDbConnection(MySqlConnection con)
+        {
+            con.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String titleName = textBox1.Text;
+            MySqlConnection con = connectToDb();
+
+            //String tbName = "title";
+            //String dbName = "library_system";
+            //String valColumn = "id";
+
+            //String keyColumn = "name";
+            //String toFind = "";
+            //String query = "select " + "*" + " from " + dbName + "." + tbName + " " + "where " + "name= " + "\'" + titleName + "\'" + ";";
+
+            //MySqlCommand command = new MySqlCommand(query, con);
+            try
+            {
+                //MySqlDataReader sqd = command.ExecuteReader();
+                //sqd.Read();
+                //int gotId = Convert.ToInt32(sqd[valColumn].ToString());
+                //sqd.Close();
+                String query2 = "select library_system.book.id,library_system.title.name as title, library_system.author.name as author,library_system.book.publish_date, library_system.book.batch_number, library_system.book.price,library_system.book.mode as mode, library_system.genre.name as genre, library_system.artifact.name as artifact from library_system.book join library_system.author join library_system.title join library_system.genre join library_system.artifact on book.title_id = title.id and book.author_id = author.id and book.genre_id = genre.id and book.artifact_id = artifact.id where title.name = " + "\'" + titleName + "\'" + ";";
+                MySqlDataAdapter command2 = new MySqlDataAdapter(query2, con);
+                DataSet gatherData = new DataSet();
+                command2.Fill(gatherData);
+                dataGridView1.DataSource = gatherData.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                closeDbConnection(con);
+            }
+            closeDbConnection(con);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String genreName = textBox2.Text;
+            MySqlConnection con = connectToDb();
+
+            //String tbName = "genre";
+            //String dbName = "library_system";
+            //String valColumn = "id";
+
+            //String keyColumn = "name";
+            //String toFind = "";
+            //String query = "select " + "*" + " from " + dbName + "." + tbName + " " + "where " + "name= " + "\'" + genreName + "\'" + ";";
+
+            //MySqlCommand command = new MySqlCommand(query, con);
+            try
+            {
+              //  MySqlDataReader sqd = command.ExecuteReader();
+              //  sqd.Read();
+              //  int gotId = Convert.ToInt32(sqd[valColumn].ToString());
+              //  sqd.Close();
+                String query2 = "select library_system.book.id,library_system.title.name as title, library_system.author.name as author,library_system.book.publish_date, library_system.book.batch_number, library_system.book.price,library_system.book.mode as mode, library_system.genre.name as genre, library_system.artifact.name as artifact from library_system.book join library_system.author join library_system.title join library_system.genre join library_system.artifact on book.title_id = title.id and book.author_id = author.id and book.genre_id = genre.id and book.artifact_id = artifact.id where genre.name = " + "\'" + genreName + "\'" + ";";
+                MySqlDataAdapter command2 = new MySqlDataAdapter(query2, con);
+                DataSet gatherData = new DataSet();
+                command2.Fill(gatherData);
+                dataGridView1.DataSource = gatherData.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                closeDbConnection(con);
+            }
+            closeDbConnection(con);
+        }
+    }
+}
